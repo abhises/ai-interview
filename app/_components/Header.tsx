32187c240f61cfee06864d874ca4596208065c93
abhises <abhisespoudyal@gmail.com>
2025-11-08 16:18:42 +0545

    first commit

---
+++ app/_components/Header.tsx
@@ -0,0 +1,17 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+import React from "react";
+
+export const Header = () => {
+  return (
+    <div>
+      <nav className="flex w-full items-center justify-between border-t border-b border-neutral-200 px-4 py-4 dark:border-neutral-800">
+        <div className="flex items-center gap-2">
+          <Image src={"./logo.svg"} alt="logo" height={500} width={500}/>
+          {/* <h1 className="text-base font-bold md:text-2xl">AI Interview</h1> */}
+        </div>
+        <Button>Get Started</Button>
+      </nav>
+    </div>
+  );
+};
